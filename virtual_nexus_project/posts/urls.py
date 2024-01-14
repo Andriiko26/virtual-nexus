cef0272dee521ac47279c8eab24e10b299ca52c2
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-14 12:22:31 +0200

    added create post view to urls

--- virtual_nexus_project/posts/urls.py
+++ virtual_nexus_project/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostsListView, PostDetailView
+from .views import PostsListView, PostDetailView, CreatePostView
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='post-home'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post-detail')
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('create_post/', CreatePostView.as_view(), name='post-create')
 ]
