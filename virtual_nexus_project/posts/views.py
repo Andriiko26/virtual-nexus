98d860138bd504d259c1e9b8e28d9264c685135e
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-14 12:21:50 +0200

    add cretate post view

--- virtual_nexus_project/posts/views.py
+++ virtual_nexus_project/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View 
 from .models import Post
+from .forms import PostForm
 
 class PostsListView(View):
     template_name = 'posts/index.html'
@@ -21,4 +22,23 @@ class PostDetailView(View):
             'post': post,
         }
         return render(request, self.template_name, context)
-        
+
+class CreatePostView(View):
+    template_name = 'posts/post_create.html'
+    form_class = PostForm
+
+    def get(self, request):
+        form = self.form_class()
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post-home')
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
