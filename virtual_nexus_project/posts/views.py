ce4369805b5c4b9b1a3e82e1d7973e39647e7a62
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-14 08:47:02 +0200

    added detail post view and change posts list view

--- virtual_nexus_project/posts/views.py
+++ virtual_nexus_project/posts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View 
+from .models import Post
 
 class PostsListView(View):
-    template_name = 'index.html'
+    template_name = 'posts/index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        posts = Post.objects.all()
+        context={
+            'posts':posts,
+        }
+        return render(request, self.template_name, context)
+
+class PostDetailView(View):
+    template_name = 'posts/post_detail.html'
+    
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        context = {
+            'post': post,
+        }
+        return render(request, self.template_name, context)
+        
