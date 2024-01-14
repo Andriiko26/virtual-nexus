eed87c210942bee9dd660419573230baa1f5e06a
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-14 09:05:44 +0200

    added views to urls.py

--- virtual_nexus_project/posts/urls.py
+++ virtual_nexus_project/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostsListView
+from .views import PostsListView, PostDetailView
 
 urlpatterns = [
-    path('', PostsListView.as_view(), name='home')
+    path('', PostsListView.as_view(), name='post-home'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail')
 ]
