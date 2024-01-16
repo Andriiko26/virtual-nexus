8eb239b72ea5536d77fbc0718afc3d0ecb781996
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-16 07:50:38 +0200

    change from bigint to uuid

--- virtual_nexus_project/posts/models.py
+++ virtual_nexus_project/posts/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import uuid
 
 class Post(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default= uuid.uuid4,
+        editable=False,
+    )
     title = models.CharField(max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
--- virtual_nexus_project/posts/urls.py
+++ virtual_nexus_project/posts/urls.py
@@ -3,6 +3,6 @@ from .views import PostsListView, PostDetailView, CreatePostView
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='post-home'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('<uuid:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('create_post/', CreatePostView.as_view(), name='post-create')
 ]
