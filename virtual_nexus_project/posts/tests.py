419a5618d536174db596ba5f43e135f0a3810b15
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-19 20:32:15 +0200

    added test for post-list

--- virtual_nexus_project/posts/tests.py
+++ virtual_nexus_project/posts/tests.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+
+class PostTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            username='testuser123',
+            email='testuseremail@gmail.com',
+            password='verysecurepassword123',   
+        )
+        self.post = Post.objects.create(
+            title='the best post ever',
+            body='body of the best post ever',
+            author=self.user,
+        )
+
+    def test_post_listing(self):
+
+        self.assertEqual(f'{self.post.title}', 'the best post ever')
+        self.assertEqual(f'{self.post.body}', 'body of the best post ever')
+        self.assertEqual(f'{self.post.author}', f'{self.user}')
+    
+    def test_post_listing_view(self):
+
+        response = self.client.get(reverse('post-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/index.html')
+    
