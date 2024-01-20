9a63be1141fc9ab387100e1b8f57aec1adfcfd57
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-20 17:12:30 +0200

    change signals

--- virtual_nexus_project/users/signals.py
+++ virtual_nexus_project/users/signals.py
@@ -4,6 +4,12 @@ from allauth.account.signals import user_signed_up
 from django.contrib.auth.models import User
 from .models import UserProfile
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and not UserProfile.objects.filter(user=instance).exists():
+        UserProfile.objects.create(user=instance)
+
 @receiver(user_signed_up)
-def create_user_profile(request, user):
-    User.objects.create(user=user)
+def create_user_profile_after_signup(request, user, **kwargs):
+    if not UserProfile.objects.filter(user=user).exists():
+        UserProfile.objects.create(user=user)
