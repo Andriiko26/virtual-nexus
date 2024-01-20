63f4138a6c479f5c344aacbf79fa703fe2330457
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-20 15:40:11 +0200

    added user profile model

--- virtual_nexus_project/users/models.py
+++ virtual_nexus_project/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class UserProfile(models.Model):
+    
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
