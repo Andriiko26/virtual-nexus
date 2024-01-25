11f649813cd224590fe63288c0e7a508fbc50541
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-25 18:15:05 +0200

    added test for profile page

--- virtual_nexus_project/users/tests.py
+++ virtual_nexus_project/users/tests.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
 from django.test import TestCase
+from .models import UserProfile
 
-# Create your tests here.
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser123',
+            email='testuseremail@gmail.com',
+            password='verysecurepassword123',   
+        )
+        self.user_profile = UserProfile.objects.get_or_create (
+            user=self.user,
+            defaults={'bio': 'Test bio.'}
+        )
+        
+
+    def test_user_profile_page(self):
+
+        #set up for this test
+        self.client.login(email='testuseremail@gmail.com', password='verysecurepassword123')
+        url = reverse('profile')
+        response = self.client.get(url)
+        
+        #exacly test
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile/profile.html')
+        self.assertContains(response, 'Username: testuser123', html=True)
+        self.assertContains(response, '<h2>User Profile</h2>', html=True)
+        self.assertNotContains(response, '<form method="post" enctype="multipart/form-data">', html=True)
+
+    
