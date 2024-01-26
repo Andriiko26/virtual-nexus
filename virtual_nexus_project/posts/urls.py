354f8e3f98c06421d0c6b99f58b75586f18c634d
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-26 17:38:49 +0200

    added searching url

--- virtual_nexus_project/posts/urls.py
+++ virtual_nexus_project/posts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import PostsListView, PostDetailView, CreatePostView
+from .views import (
+    PostsListView, 
+    PostDetailView, 
+    CreatePostView,
+    PostSearch,
+)
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='post-home'),
     path('<uuid:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('create_post/', CreatePostView.as_view(), name='post-create')
+    path('create_post/', CreatePostView.as_view(), name='post-create'),
+    path('search/', PostSearch.as_view(), name='post-search'),
 ]
