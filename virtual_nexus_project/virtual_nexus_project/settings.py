44a5719e90896ce313c96cddec8df12f8c394066
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-01-27 07:58:24 +0200

    move static files

--- virtual_nexus_project/virtual_nexus_project/settings.py
+++ virtual_nexus_project/virtual_nexus_project/settings.py
@@ -97,13 +97,7 @@ TEMPLATES = [
         },
     },
 ]
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_FINDERS = [ 
-    "django.contrib.staticfiles.finders.FileSystemFinder", 
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder", 
-]
+
 
 WSGI_APPLICATION = 'virtual_nexus_project.wsgi.application'
 
@@ -158,6 +152,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [ 
+    "django.contrib.staticfiles.finders.FileSystemFinder", 
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder", 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
