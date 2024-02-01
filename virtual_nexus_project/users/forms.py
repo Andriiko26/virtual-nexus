3db77f7c8e91378bed19b141a7e9937508835e53
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-01 17:07:18 +0200

    updated form

--- virtual_nexus_project/users/forms.py
+++ virtual_nexus_project/users/forms.py
@@ -3,6 +3,22 @@ from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
     
+    username = forms.CharField(max_length=50)
+
     class Meta:
         model = UserProfile
         fields = ['bio', 'avatar']
+    
+    def __init__(self, *args, **kargs) -> None:
+        super(UserProfileForm, self).__init__(*args, **kargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter your username'
+
+    def save(self, commit=True):
+        user_profile = super(UserProfileForm, self).save(commit=False)
+        user_profile.user.username = self.cleaned_data['username']
+        if commit:
+            user_profile.user.save()    
+            user_profile.save()
+
+        return user_profile
