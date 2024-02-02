aa60df1268a49fd9e98fd36a164e85745acc2bf7
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-02 07:46:38 +0200

    updated post model

--- virtual_nexus_project/posts/models.py
+++ virtual_nexus_project/posts/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     body = models.TextField()
+    photo = models.ImageField(upload_to='post_photos/', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -20,6 +21,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail', args=(str(self.id),))
     
+
+
+
 class Comment(models.Model):
     
     author = models.ForeignKey(User, on_delete=models.CASCADE)
