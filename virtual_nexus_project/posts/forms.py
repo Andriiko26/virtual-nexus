b7cda67df6dee9bef6a7ac77b6bb04363fa9a48c
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-03 09:59:56 +0200

    added form for tags

--- virtual_nexus_project/posts/forms.py
+++ virtual_nexus_project/posts/forms.py
@@ -2,13 +2,30 @@ from django import forms
 from .models import (
     Post,
     Comment,
+    Tag,
 )
 
 class PostForm(forms.ModelForm):
-    
+    tags = forms.CharField(max_length=100, required=False)
+
     class Meta:
         model = Post
-        fields = ['title', 'body', 'photo']
+        fields = ['title', 'body', 'photo', 'tags']
+
+    def clean_tags(self):
+        raw_tags = self.cleaned_data['tags']
+        tag_texts = [tag.strip().lower() for tag in raw_tags.split(',') if tag.strip()]
+        tags = [Tag.objects.get_or_create(tag_text=tag)[0] for tag in tag_texts]
+
+        return tags
+
+    def save(self, commit=True):
+        post = super().save(commit=False)
+        if commit:
+            post.save()
+            post.tags.set(self.cleaned_data['tags'])
+        return post
+
 
 class CommentForm(forms.ModelForm):
 
