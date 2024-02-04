75529e18640ed665efe2a87e325a5682bc5f227f
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-04 17:43:33 +0200

    created logic for api

--- virtual_nexus_project/api/views.py
+++ virtual_nexus_project/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from posts.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListView(APIView):
+    """Retruns all post data 
+    """
+
+    def get(self, request, *args, **kwargs):
+
+        posts = Post.objects.all()
+        serializer  = PostSerializer(posts, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+        
