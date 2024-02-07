2ecc7106fdb2ead49fc55cfc03d4a57d54a10a3e
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-07 19:04:42 +0200

    added token creating

--- virtual_nexus_project/api/urls.py
+++ virtual_nexus_project/api/urls.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView
 
@@ -5,4 +6,5 @@ urlpatterns = [
     path('', PostListView.as_view()),
     path('<uuid:pk>/', PostDetailView.as_view()),
     path('create/', PostCreateView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
