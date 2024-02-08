3fda14e2c320300359259c3d179f2ed75fd87e08
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-08 18:52:44 +0200

    added like to urls

--- virtual_nexus_project/api/urls.py
+++ virtual_nexus_project/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostLikeView
 
 urlpatterns = [
     path('', PostListView.as_view()),
     path('<uuid:pk>/', PostDetailView.as_view()),
     path('create/', PostCreateView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('<uuid:pk>/like', PostLikeView.as_view()),
 ]
