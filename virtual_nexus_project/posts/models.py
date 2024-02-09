2a4a4a1a45baa358bdbd6995a83c4163dc7ffada
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-09 14:39:45 +0200

    updated model to markdown field

--- virtual_nexus_project/posts/models.py
+++ virtual_nexus_project/posts/models.py
@@ -1,3 +1,4 @@
+from markdownx.models import MarkdownxField
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -19,7 +20,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
-    body = models.TextField()
+    body = MarkdownxField()
     photo = models.ImageField(upload_to='post_photos/', blank=True, null=True)
     tags = models.ManyToManyField(Tag, related_name='posts')
 
