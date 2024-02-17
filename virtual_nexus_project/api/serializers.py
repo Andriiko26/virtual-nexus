aae0eaadeca7cdab895da92c44f94d9ae45a7cb6
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-02-17 10:09:15 +0200

    created serializer for displaying comments

--- virtual_nexus_project/api/serializers.py
+++ virtual_nexus_project/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from posts.models import Post
+from posts.models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields  = ['id','title', 'body', 'tags']
+
+class CommentListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Comment
+        fields = ['text', 'author', 'created_at']
